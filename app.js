7f321d65ea06dcf21022a3df12e94e3366be643d
khenryjoe <joseph.ketterer@gmail.com>
2019-02-02 02:24:56 +0000

    add articles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -29,6 +30,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 
 // home route
@@ -48,6 +53,8 @@ app.get('/', function(req, res){
 
 });
 
+
+
 app.listen(5000, function(){
     console.log('server started on port 5000');
 });
@@ -56,4 +63,23 @@ app.get('/articles/add', function(req, res){
     res.render('add_article', {
         title: 'Add article'
     });
+});
+
+//add submit post route
+app.post('/articles/add', function(req,res){
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            console.log('worked');
+            res.redirect('/');
+        }
+    })
+
+
 });
