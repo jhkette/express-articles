e64cda3b01b41af5438c3e577d3e3fba7d891d44
khenryjoe <joseph.ketterer@gmail.com>
2019-02-02 01:44:08 +0000

    connecting to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+
+//check connection
+db.once('open', function(){
+    console.log('Connected to MongoDb');
+})
+
+
+//check for db errors
+
+db.on('error', function(err){
+    console.log(err)
+});
 
 //init app
 const app = express();
+
+let Article = require('./models/article');
 
 
 // load view engine
@@ -15,9 +33,19 @@ app.set('view engine', 'pug');
 
 // home route
 app.get('/', function(req, res){
-    res.render('index',{
-        title: 'Articles'
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        }
+        else{
+        res.render('index',{
+            title: 'Articles',
+            articles: articles
+
+        });
+       }
     });
+
 });
 
 app.listen(5000, function(){
