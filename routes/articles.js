395e9d8363074e0a8de2843a2708eeed3cfdc00f
khenryjoe <joseph.ketterer@gmail.com>
2019-02-04 22:23:37 +0000

    add routes

---
+++ routes/articles.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+
+// Article Model
+let Article = require('../models/article');
+
+
+router.get('/:id', function(req, res) {
+    Article.findById(req.params.id, function(err, article) {
+        res.render('article', {
+            article: article
+        })
+    });
+});
+
+router.get('/:id', function(req, res) {
+    Article.findById(req.params.id, function(err, article) {
+        res.render('edit_article', {
+            article: article
+        })
+    });
+});
+
+router.get('/add', function(req, res){
+    res.render('add_article', {
+        title: 'Add article'
+    });
+});
+
+router.post('/add', function(req,res){
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('author', 'Author is required').notEmpty();
+    req.checkBody('body', 'Body is required').notEmpty();
+
+    //Get errors
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.render('add_article',{
+
+            title: 'Add article',
+            errors: errors,
+        } );
+    }else{
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+
+
+        article.save(function(err){
+            if(err){
+                console.log(err)
+                return;
+            }else{
+                req.flash('succes', 'Article added');
+                res.redirect('/');
+            }
+        })
+    }
+});
+
+
+//update submit
+router.post('/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article Updated');
+      res.redirect('/');
+    }
+  });
+});
+
+
+router.get('/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+
+    res.render('edit_article', {
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
+
+router.delete('/:id', function(req, res){
+
+  let query = {_id:req.params.id}
+  console.log(query);
+
+  Article.findById(req.params.id, function(err, article){
+
+      Article.remove(query, function(err){
+        if(err){
+          console.log(err);
+        }
+        res.send('Success');
+      });
+
+  });
+});
+
+module.exports = router;
